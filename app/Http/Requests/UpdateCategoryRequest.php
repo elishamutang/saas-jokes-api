f8820effff32affe847b66835e75d8158efd0c81
elishamutang <elishamutang@outlook.com>
2025-09-30 00:31:07 +0800

    update: Updated store and update category requests.

--- app/Http/Requests/UpdateCategoryRequest.php
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -11,7 +11,7 @@ class UpdateCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,16 @@ class UpdateCategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => ['sometimes', 'required', 'string'],
+            'description' => ['sometimes', 'required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'Title cannot be empty.',
+            'description.required' => 'Description cannot be empty.',
         ];
     }
 }
