a4e2a5f6473ed18e75d6df591ff7dbd56d62faf8
elishamutang <elishamutang@outlook.com>
2025-10-04 15:25:04 +0800

    update: Updated seeder file.

--- database/seeders/JokeSeeder.php
+++ database/seeders/JokeSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Joke;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
 
 class JokeSeeder extends Seeder
 {
@@ -14,65 +15,66 @@ class JokeSeeder extends Seeder
      */
     public function run(): void
     {
+        // Randomise the user
+        $users = User::all()->pluck('id')->toArray();
+
         $seedJokes = [
             [
                 'title' => 'Skeleton Fight',
                 'content' => "Why don't skeletons fight each other? ".
                            "Because they don't have the guts.",
-                'user_id' => 100,
-                'categories' => ['Skeleton'],
+                'user_id' => $users[array_rand($users)],
+                'categories' => ['Skeleton', 'Maths'],
             ],
             [
                 'title' => 'Pirate Maths',
                 'content' => 'What type of Maths are pirates best at?'.
                            'Algebra. Because they are good at finding X.',
-                'user_id' => 100,
+                'user_id' => $users[array_rand($users)],
                 'categories' => ['Pirate', 'Maths'],
             ],
             [
                 'title' => 'SQL Bar',
                 'content' => 'An SQL query walks into a bar, sees two tables, and asks: "Can I join you?"',
-                'user_id' => 100,
-                'categories' => ['SQL', 'Database', 'Programming'],
+                'user_id' => $users[array_rand($users)],
+                'categories' => ['SQL', 'Database', 'Programming', 'Maths'],
             ],
             [
                 'title' => 'Light Bulb',
                 'content' => 'How many programmers does it take to change a light bulb? None. That’s a hardware problem.',
-                'user_id' => 100,
-                'categories' => ['Hardware', 'Programming', 'Devices'],
+                'user_id' => $users[array_rand($users)],
+                'categories' => ['Hardware', 'Programming', 'Devices', 'Maths'],
             ],
             [
                 'title' => 'Java Divorce',
                 'content' => 'Why did the two Java methods get a divorce? Because they had constant arguments.',
-                'user_id' => 100,
-                'categories' => ['Java', 'Programming'],
+                'user_id' => $users[array_rand($users)],
+                'categories' => ['Java', 'Programming', 'Maths'],
             ],
             [
                 'title' => 'Halloween vs Christmas',
                 'content' => 'Why do programmers always mix up Christmas and Halloween? Because Dec 25 == Oct 31.',
-                'user_id' => 100,
-                'categories' => ['Programming', 'Dates', 'Logic'],
+                'user_id' => $users[array_rand($users)],
+                'categories' => ['Programming', 'Dates', 'Logic', 'Maths'],
             ],
             [
                 'title' => 'Cache Bankruptcy',
                 'content' => 'Why did the edge server go bankrupt? Because it ran out of cache.',
-                'user_id' => 100,
-                'categories' => ['Server', 'Caching', 'Programming'],
+                'user_id' => $users[array_rand($users)],
+                'categories' => ['Server', 'Caching', 'Programming', 'Maths'],
             ],
         ];
-
-        $users = User::all()->pluck('id', 'id')->toArray();
 
         foreach ($seedJokes as $seedJoke) {
 
             $categoryList = $seedJoke['categories'] ?? ['Unknown'];
             unset($seedJoke['categories']);
 
-            $joke = Joke::updateOrCreate([
-                'title' => $seedJoke['title'],
-                'content' => $seedJoke['content'],
-                'user_id' => $users[array_rand($users)],
-            ]);
+            $joke = Joke::updateOrCreate(
+                ['title' => $seedJoke['title']],
+                ['content' => $seedJoke['content'],
+                    'user_id' => $seedJoke['user_id']]
+            );
 
             foreach ($categoryList as $category) {
                 Category::updateOrCreate(['title' => $category]);
