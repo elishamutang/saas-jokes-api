0638056a8fe40979f370db3f38eab3c06bbb5dfe
elishamutang <elishamutang@outlook.com>
2025-10-05 15:54:09 +0800

    update: Perform user authorization in authorize method.

--- app/Http/Requests/StoreCategoryRequest.php
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Responses\ApiResponse;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCategoryRequest extends FormRequest
@@ -11,7 +12,7 @@ class StoreCategoryRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return auth()->user()->hasPermissionTo('create a category');
     }
 
     /**
