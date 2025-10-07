f0531d21e1569c08dd7b26684390c049250a3cb4
elishamutang <elishamutang@outlook.com>
2025-10-07 11:40:35 +0800

    misc: Added status check as middleware.

---
+++ app/Http/Middleware/CheckUserNotSuspended.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Responses\ApiResponse;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckUserNotSuspended
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if ($request->user()->status === 'suspended') {
+            return ApiResponse::error([], "Please reset your password.", 400);
+        }
+
+        return $next($request);
+    }
+}
