fa904e4facabf8ffc98dacb45c8434f1626efb10
elishamutang <elishamutang@outlook.com>
2025-10-08 00:22:38 +0800

    feat: Added ProfileController and tests.

--- app/Http/Controllers/Api/v2/ProfileController.php
+++ app/Http/Controllers/Api/v2/ProfileController.php
@@ -3,26 +3,41 @@
 namespace App\Http\Controllers\Api\v2;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\UpdateUserRequest;
 use App\Responses\ApiResponse;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class ProfileController extends Controller
 {
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateUserRequest $request): JsonResponse
+    public function update(Request $request): JsonResponse
     {
-        // Validate request
-        $validated = $request->validated();
-
         // Get authenticated user
         $user = $request->user();
 
+        // Validate request
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => ['sometimes', 'required', 'string'],
+                'email' => ['sometimes', 'required', 'email', Rule::unique('users')->ignore($user)],
+                'password' => ['sometimes', 'required', Password::min(6), 'confirmed'],
+            ]
+        );
+
+        if ($validator->fails()) {
+            return ApiResponse::error([
+                'errors' => $validator->errors()
+            ], "Please fix the validation errors.", 400);
+        }
+
         // Update user
-        $user->update($validated);
+        $user->update($validator->validated());
         return ApiResponse::success($user, "Profile updated successfully");
     }
 
