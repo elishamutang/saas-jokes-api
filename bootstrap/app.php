080633083a843338e8284e27d5eaec4ecd4e7279
elishamutang <elishamutang@outlook.com>
2025-10-08 23:50:23 +0800

    update: Updated routing.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,13 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function (){
-            Route::middleware('api')
-                ->prefix('api/v1')
-                ->group(base_path('routes/api_v1.php'));
-
-            \Illuminate\Support\Facades\Route::middleware('api')
-                ->prefix('api/v2')
-                ->group(base_path('routes/api_v2.php'));
+//            \Illuminate\Support\Facades\Route::middleware('api')
+//                ->prefix('api/v2')
+//                ->group(base_path('routes/api_v2.php'));
 
             /* Add further API versions as required */
 
