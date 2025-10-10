6b3987d11ae372982aa62420ac70d572e2b38985
elishamutang <elishamutang@outlook.com>
2025-10-10 21:47:17 +0800

    update: Modified middleware priority.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -35,10 +35,22 @@ return Application::configure(basePath: dirname(__DIR__))
             'user.status' => CheckUserStatus::class,
         ]);
 
-        $middleware->prependToPriorityList(
-            before: \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            prepend: CustomAuthHeaderForSanctumToken::class,
-        );
+        // Explicitly sort middleware
+        $middleware->priority([
+            CustomAuthHeaderForSanctumToken::class,
+            \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            \Illuminate\Routing\Middleware\ThrottleRequestsWithRedis::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Contracts\Auth\Middleware\AuthenticatesRequests::class,
+            \Illuminate\Auth\Middleware\Authorize::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function(ModelNotFoundException $error, Request $request){
